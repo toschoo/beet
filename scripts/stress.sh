5332591d1be0047515341a8ba1bbd5ea5bd301aa
toschoo <tobias.schoofs@gmx.net>
2018-05-28 14:47:19 +0200

    [develop] more stress

--- scripts/stress.sh
+++ scripts/stress.sh
@@ -7,6 +7,18 @@ fi
 
 echo "RUNNING STRESS TESTS"
 
+test/stress/riderstress -threads 1 -max 1000
+if [ $? -ne 0 ]
+then
+	exit 1
+fi
+
+test/stress/riderstress -threads 2 -max 1000
+if [ $? -ne 0 ]
+then
+	exit 1
+fi
+
 test/stress/riderstress -threads 10 -max 100
 if [ $? -ne 0 ]
 then
